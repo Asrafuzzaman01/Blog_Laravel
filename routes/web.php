28808e8dd02dcff3b4a4003931fbefa2b60fd16d
Asrafuzzaman01 <asrafuzzaman.cse.siu@gmail.cvom>
2022-09-19 01:11:44 +0600

    all page html setup

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+/* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
+// Route::get('/', function () {
+// return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('website.index');
+});
+Route::get('/about', function () {
+    return view('website.about');
+});
+Route::get('/category', function () {
+    return view('website.category');
+});
+Route::get('/contact', function () {
+    return view('website.contact');
+});
+Route::get('/post', function () {
+    return view('website.post');
 });
